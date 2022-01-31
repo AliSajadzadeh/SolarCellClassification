65a25d7266d11944eb3c7fb21a56e6fd043412cb
Ali Sajadzadeh <ali.sajadzadeh@g,ail.com>
2022-01-31 17:24:24 +0100

    model has been modified

--- scr_to_implement/model.py
+++ scr_to_implement/model.py
@@ -9,12 +9,12 @@ class ResBlock(nn.Module):
         self.stride = stride
         self.in_channels = in_channel
         self.out_channels = out_channel
-        self.conv1 = nn.Conv2d(in_channel,out_channel,kernel_size=3,stride=stride)
+        self.conv1 = nn.Conv2d(in_channel,out_channel,kernel_size=3,stride=stride,padding=1)
         self.bn1 = nn.BatchNorm2d(out_channel)
         self.relu = nn.ReLU()
-        self.conv2 = nn.Conv2d(out_channel,out_channel,kernel_size=3,stride=None)
+        self.conv2 = nn.Conv2d(out_channel,out_channel,kernel_size=3,padding=1)
         self.bn2 = nn.BatchNorm2d(out_channel)
-        self.conv_matching = nn.Conv2d(in_channel,out_channel,kernel_size=1)
+        self.conv_matching = nn.Conv2d(in_channel,out_channel,kernel_size=1,stride=stride)
         self.bn_matching = nn.BatchNorm2d(out_channel)
         self.input_matching = None
 
@@ -30,9 +30,9 @@ class ResBlock(nn.Module):
         x = self.relu(x)
         x = self.conv2(x)
         x = self.bn2(x)
-        skipped_connection = ResBlock.layer_matching(self.input)
+        skipped_connection = ResBlock.layer_matching(self,self.input)
         x += skipped_connection
-        x =self.relu
+        x =self.relu(x)
         return x
 
 
@@ -42,7 +42,7 @@ class ResBlock(nn.Module):
     def layer_matching(self,input):
         if self.stride != 1 or self.in_channels != self.out_channels:
             self.input_matching = nn.Sequential(self.conv_matching,self.bn_matching)
-            self.output = self.input(input)
+            self.output = self.input_matching(input)
         else:
             self.output = input
         return self.output
@@ -58,7 +58,7 @@ class ResNet(nn.Module):
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=3,stride=2)
         # toget the better result add 2D averagepooling and then apply flatten
-        #self.globalaverage = nn.AvgPool2d()
+        self.globalaverage = nn.AdaptiveAvgPool2d
         #self.flatten = ????????????????????????????????????????????????????????????????????????
         self.FC = nn.Linear(512,2)
         self.prediction = nn.Sigmoid()
@@ -66,6 +66,7 @@ class ResNet(nn.Module):
         self.ResBlock_object64_128 = ResBlock(64, 128, 2)
         self.ResBlock_object128_256 = ResBlock(128, 256, 2)
         self.ResBlock_object256_512 = ResBlock(256,512, 2)
+
 
 
     def forward(self, x):
@@ -77,7 +78,8 @@ class ResNet(nn.Module):
         x = self.ResBlock_object64_128.forward(x)
         x = self.ResBlock_object128_256.forward(x)
         x = self.ResBlock_object256_512.forward(x)
-        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.mean([2,3])
+        #x = F.adaptive_avg_pool2d(x, (1, 1))
         x = self.FC(x)
         x = self.prediction(x)
         return x
